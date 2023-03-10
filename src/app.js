6cb4b8e4e31d15d27a60d7a1983ee8499e0653a8
NettoJM <nettojm86@gmail.com>
2023-03-10 15:49:48 -0300

    Hello World

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 // src/app.js
 const express = require('express');
 
+const OK = 200;
+// const INTERNAL_SERVER_ERROR = 500;
+// const NOT_FOUND = 404;
+
 const app = express();
+
+app.get('/', (req, res) => res.status(OK).json({
+  message: 'Hello World!'
+}));
 
 module.exports = app;
