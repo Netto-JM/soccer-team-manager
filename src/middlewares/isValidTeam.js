89e33a26657577598053477362ef5762c3345185
NettoJM <nettojm86@gmail.com>
2023-03-19 21:41:00 -0300

    Modified "isValidTeam" middleware to give more precise error messages

--- src/middlewares/isValidTeam.js
+++ src/middlewares/isValidTeam.js
@@ -1,12 +1,14 @@
 const HTTP_BAD_REQUEST_STATUS = 400;
 
 const isValidTeam = (req, res, next) => {
-  const requiredProperties = ['nome', 'sigla'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    next();
-  } else {
-    res.sendStatus(HTTP_BAD_REQUEST_STATUS);
-  }
+  const { nome, sigla } = req.body;
+  if (!nome) return res.status(400).json({
+    message: 'The "nome" field is required'
+  });
+  if (!sigla) return res.status(400).json({
+    message: 'The "sigla" field is required'
+  });
+  next();
 };
 
 module.exports = { isValidTeam };
